b13617ed1d72a08c184c74827321027add92a0ad
Bruno Paulino <bruno@bpaulino.com>
2023-08-21 00:28:46 +0200

    Execute DB migration during container startup

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# docker entrypoint script.
+
+# start the elixir app
+echo "Executing Ecto migrations..."
+eval "/app/bin/rinha_elixir eval \"Turbo.Release.migrate\""
+
+# start the elixir application
+echo "Starting Rinha de Backend Elixir app..."
+
+# the `exec` command is special because when used to execute a 
+# command like running our service, it will replace the parent process 
+# with the new process. 
+# Now when Docker sends the SIGTERM signal to the process id,
+# our Elixir app will trap the SIGTERM and gracefully shutting down.
+exec "/app/bin/server" "start"
