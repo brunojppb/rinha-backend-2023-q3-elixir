bbccd5ca708b2b9dbaa417384a991234d20eb76e
Bruno Paulino <bruno@bpaulino.com>
2023-08-21 09:55:00 +0200

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RinhaElixir.MixProject do
   def project do
     [
       app: :rinha_elixir,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
